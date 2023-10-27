5c4ee63539b9675b5d55eacfcf4c09b9c83f31fd
Nicholas Arsenault <nickarsenault18@gmail.com>
2023-10-27 12:59:42 -0400

    Update main.py

--- Day1-BandNameGenerator/main.py
+++ Day1-BandNameGenerator/main.py
@@ -1,1 +1,4 @@
-
+print("Welcome to the Band Name Generator.")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is the name of your first pet?\n")
+print("Your band name is: " + city + " " + pet + ".")
