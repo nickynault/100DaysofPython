4505eb4571767e6aac25974239222ae133093b0e
Nicholas Arsenault <nickarsenault18@gmail.com>
2023-11-19 20:02:26 -0500

    Create main.py

---
+++ Day16-CoffeeMakerOOP/main.py
@@ -0,0 +1,24 @@
+# MY WORK
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+isOn = True
+
+
+while isOn:
+    options = menu.get_items()
+    choice = input(f"What would you like?? ({options}): ").lower()
+    if choice == "off":
+        isOn = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
