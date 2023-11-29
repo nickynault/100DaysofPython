fc4dbe98a492104cfb52bef42fd51a61b1fd4fc5
Nicholas Arsenault <nickarsenault18@gmail.com>
2023-11-28 21:16:42 -0500

    Create main.py

---
+++ Day20&21-SnakeGame/main.py
@@ -0,0 +1,26 @@
+from turtle import Screen
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "w")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.right, "d")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(.1)
+
+    snake.move()
+
+screen.mainloop()
