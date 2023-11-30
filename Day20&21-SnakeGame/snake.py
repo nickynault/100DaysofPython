130e095804b584b6579f97731693c338d3a95c7a
Nicholas Arsenault <nickarsenault18@gmail.com>
2023-11-29 19:47:11 -0500

    Update snake.py

--- Day20&21-SnakeGame/snake.py
+++ Day20&21-SnakeGame/snake.py
@@ -17,11 +17,14 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
 
     def move(self):
         for seg in range(len(self.segments) - 1, 0, -1):
@@ -45,3 +48,6 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
+
+    def grow(self):
+        self.add_segment(self.segments[-1].position())
