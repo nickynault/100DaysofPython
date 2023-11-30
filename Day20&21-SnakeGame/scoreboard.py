ffb1f2cb1e05344ae0a70d9275faae13f3fc52a4
Nicholas Arsenault <nickarsenault18@gmail.com>
2023-11-29 19:47:36 -0500

    Update scoreboard.py

--- Day20&21-SnakeGame/scoreboard.py
+++ Day20&21-SnakeGame/scoreboard.py
@@ -1,1 +1,23 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.hideturtle()
+        self.penup()
+        self.goto(0, 270)
+        self.score = 0
+        self.write(f"Score = {self.score}", align="center", font=("Arial", 16, "normal"))
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score = {self.score}", align="center", font=("Arial", 16, "normal"))
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=("Arial", 24, "normal"))
 
