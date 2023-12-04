2e5e517b4cc711b9e017f63a84775287cb79be89
Nicholas Arsenault <nickarsenault18@gmail.com>
2023-12-03 21:34:32 -0500

    Create ball.py

---
+++ Day22-Pong/ball.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+
+    def moving(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce(self, is_paddle):
+        if not is_paddle:
+            self.y_move *= -1
+        elif is_paddle:
+            self.x_move *= -1
+
+    def reset_position(self):
+        self.goto(0, 0)
