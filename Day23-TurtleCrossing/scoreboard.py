a5cf606598c849cb264b0216f0aa54fef7c4749e
Nicholas A <nickarsenault18@gmail.com>
2023-12-04 17:56:02 -0500

    Create scoreboard.py

---
+++ Day23-TurtleCrossing/scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.goto(-200, 250)
+        self.level = 0
+        self.write(f"Level: {self.level}", align="center", font=FONT)
+
+    def level_up(self):
+        self.clear()
+        self.level += 1
+        self.write(f"Level: {self.level}", align="center", font=FONT)
+
+    def game_over(self):
+        self.clear()
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
