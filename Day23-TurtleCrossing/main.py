fd6d3f6e51cea01aacd5fae8366bd996d61a671c
Nicholas A <nickarsenault18@gmail.com>
2023-12-04 17:55:30 -0500

    Update main.py

--- Day23-TurtleCrossing/main.py
+++ Day23-TurtleCrossing/main.py
@@ -1,1 +1,41 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+scoreboard = Scoreboard()
+car_manager = CarManager()
+
+screen.listen()
+
+screen.onkeypress(player.move, "w")
+screen.onkeypress(player.move, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # Detect successful crossing
+    if player.is_finished():
+        player.reset_position()
+        car_manager.level_up()
+        scoreboard.level_up()
+
+screen.mainloop()
 
