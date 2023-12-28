f16f6f891ccc13f04658712275b6a613eafc48d6
Nicholas A <nickarsenault18@gmail.com>
2023-12-27 21:06:27 -0500

    Update main.py
    Added search function with error handles, and replaced the txt file updates/creation with JSON, and imported json, and added exceptions to that as well. txt file remains but is unused

--- Day29&30-PasswordManager/main.py
+++ Day29&30-PasswordManager/main.py
@@ -2,15 +2,23 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 FONT = ("Arial", 14, "normal")
 
 
+# ---------------------------------------------SAVING DATA--------------------------------------------------------#
 def save():
     global is_ok
     website = web_entry.get()
     email = user_entry.get()
     password = pass_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 and len(password) == 0:
         messagebox.showinfo(title="Error", message="One or more boxes have not been filled in. "
@@ -20,12 +28,28 @@ def save():
                                                               f"\nPassword: {password} \nIs it ok to save?")
 
     if is_ok:
-        with open("data.txt", "a") as data_file:
-            data_file.write(f"{website} | {email} | {password}\n")
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+
+        finally:
             web_entry.delete(0, END)
             pass_entry.delete(0, END)
 
 
+# ---------------------------------------------GENERATE PASSWORDS----------------------------------------------------#
 def gen_pass():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
@@ -42,9 +66,27 @@ def gen_pass():
     shuffle(password_list)
     password = "".join(password_list)
     pass_entry.insert(0, password)
-    pyperclip.copy(password)           # copies generated password to your clipboard
+    pyperclip.copy(password)  # copies generated password to your clipboard
 
 
+# ---------------------------------------------PASSWORD SEARCH-------------------------------------------------------#
+def find_password():
+    website = web_entry.get()
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title="Result Found", message=f"Email: {email}\n Password: {password}")
+        else:
+            messagebox.showinfo(title="No Result Found", message="Sorry, no details for this website exist.")
+    
+
+# ---------------------------------------------UI SETUP--------------------------------------------------------------#
 window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
@@ -61,11 +103,11 @@ tag2.grid(row=2, column=0)
 tag3 = Label(text="Password:", font=FONT)
 tag3.grid(row=3, column=0)
 
-web_entry = Entry(width=35)
-web_entry.grid(row=1, column=1, columnspan=2)
+web_entry = Entry(width=40)
+web_entry.grid(row=1, column=1)
 web_entry.focus()
 user_entry = Entry(width=35)
-user_entry.grid(row=2, column=1, columnspan=2)
+user_entry.grid(row=2, column=1)
 user_entry.insert(0, "user@gmail.com")
 pass_entry = Entry(width=21)
 pass_entry.grid(row=3, column=1)
@@ -74,5 +116,7 @@ pass_button = Button(text="Generate Password", font=FONT, command=gen_pass)
 pass_button.grid(row=3, column=2)
 add_button = Button(text="Add", font=FONT, width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+search_button = Button(text="Search", font=FONT, command=find_password)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
