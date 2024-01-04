58ba543dbf87577e3537698d619fe91b91ff0aec
Nicholas A <nickarsenault18@gmail.com>
2024-01-03 19:00:30 -0500

    Update main.py
    finished up. perfected timer and card transitions, added creation of new csv with words you still need to learn based on whether you check or X on options. 
    added exception for the first time the files made and for after the first time to save progress.
    (working, complete)

--- Day31-Flashcards/main.py
+++ Day31-Flashcards/main.py
@@ -5,9 +5,16 @@ import random
 import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
 current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    og_data = pandas.read_csv("data/french_words.csv")
+    to_learn = og_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
 def random_word():
@@ -18,13 +25,21 @@ def random_word():
     canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
     canvas.itemconfig(card_background, image=card_front_img)
-    app.after(3000, func=switch_cards)
+    flip_timer = app.after(3000, func=switch_cards)
 
 
 def switch_cards():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    new_data = pandas.DataFrame(to_learn)
+    new_data.to_csv("data/words_to_learns.csv", index=False)
+
+    random_word()
 
 
 app = Tk()
@@ -52,7 +67,7 @@ unknown_button.image = unknown_button_img
 unknown_button.grid(row=1, column=0)
 
 check_button_img = PhotoImage(file="images/right.png")
-check_button = Button(image=check_button_img, highlightthickness=0, bd=0, command=random_word)
+check_button = Button(image=check_button_img, highlightthickness=0, bd=0, command=is_known)
 check_button.image = check_button_img
 check_button.grid(row=1, column=1)
 
