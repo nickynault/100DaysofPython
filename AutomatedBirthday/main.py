e7e2e198400e426326f405cc59fc273f9ed8785b
Nicholas A <nickarsenault18@gmail.com>
2024-01-05 20:05:23 -0500

    Update main.py
    added auto bday wisher
    (working, complete)

--- AutomatedBirthday/main.py
+++ AutomatedBirthday/main.py
@@ -48,4 +48,30 @@
 
 ##################AUTOMATIC BIRTHDAY WISHER####################################################################################
 
+import smtplib
+import datetime as dt
+import pandas
+import random
+
+my_email = "appbreweryinfo@gmail.com"
+password = "abcd1234()"
+
+today = (dt.datetime.now().month, dt.datetime.now().day)
+data = pandas.read_csv("birthdays.csv")
+
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if today in birthdays_dict:
+    birthday_person = birthdays_dict[today]
+
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(my_email, password)
+        connection.sendmail(from_addr=my_email, to_addrs=birthday_person["email"], msg=f"Subject:Happy Birthday!\n\n"
+                                                                                       f"{contents}")
 
