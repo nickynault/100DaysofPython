717089a2ef64b682e9d5f64fff8fdbad0e843fb8
Nicholas A <nickarsenault18@gmail.com>
2024-01-07 21:33:22 -0500

    Update ui.py
    added give_feedback
    fixed background colors changing
    scorekeeping added
    end of questions accounted for
    
    (working, complete)

--- Day34-Trivia/ui.py
+++ Day34-Trivia/ui.py
@@ -40,13 +40,27 @@ class QuizInterface:
         self.window.mainloop()
         
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
     def hit_true(self):
         self.quiz.check_answer("True")
-        self.get_next_question()
 
     def hit_false(self):
         self.quiz.check_answer("False")
-        self.get_next_question()
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+
+        self.window.after(1000, self.get_next_question)
