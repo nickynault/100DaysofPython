122c1c87d20dc85e460c9c9b6b2f20f0cea09ae7
Nicholas A <nickarsenault18@gmail.com>
2024-01-07 20:23:33 -0500

    Update ui.py
    imported quizbrain, added param, added getnextquestion
    (working)

--- Day34-Trivia/ui.py
+++ Day34-Trivia/ui.py
@@ -1,13 +1,16 @@
 # ui.py (class)
 
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)\
@@ -18,6 +21,7 @@ class QuizInterface:
         self.canvas = Canvas(width=300, height=250, bg="white")
         self.question_text = self.canvas.create_text(150,
                                                      125,
+                                                     width=280,
                                                      text="Some question text",
                                                      fill=THEME_COLOR,
                                                      font=("Arial", 20, "italic"))
@@ -31,4 +35,11 @@ class QuizInterface:
         self.false_button = Button(image=false_img, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+        
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
