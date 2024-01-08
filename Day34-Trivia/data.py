47ba38d2626291e9d9d2a0cf7e69108ea6de7426
Nicholas A <nickarsenault18@gmail.com>
2024-01-07 19:49:25 -0500

    Create data.py
    initial start had 10 hardcoded questions.
    this replaces that with an actual api usage
    (working)

---
+++ Day34-Trivia/data.py
@@ -0,0 +1,13 @@
+# data.py
+
+import requests
+
+parameters = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+response = requests.get("https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+data = response.json()
+question_data = data["results"]  # this fills in our questions for the trivia game from the api
