67af8573e9f8ec8c0f979626ab5784582071b49f
Nicholas A <nickarsenault18@gmail.com>
2024-01-15 14:03:06 -0500

    Update data_manager.py
    import requests and pprint
    set sheety info
    start class
    set up destdata automatically on object creation
    getdestdata function gets data from sheety via requests and sets destdata to the prices
    pprint prints it nicely
    return the new destdata
    
    updatedestcodes grabs iatacodes per city for every city and throws them into our sheets file via sheety
    
    (working)

--- Day39&40-FlightCapstone/data_manager.py
+++ Day39&40-FlightCapstone/data_manager.py
@@ -1,4 +1,34 @@
 # This class is responsible for talking to the Google Sheet.
 
+import requests
+from pprint import pprint
+
+SHEETY_API = "https://api.sheety.co/686d9c760634acdc6102b3a36a6d462d/flightDeals/prices"
+SHEETY_NAME = "FlightDeals"
+
+
 class DataManager:
-    pass
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_API)
+        data = response.json()
+        self.destination_data = data["prices"]
+
+        pprint(self.destination_data)
+
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_API}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
