5a6f286a45fd29d08d39b3c40c53edb9dde33cf9
Nicholas A <nickarsenault18@gmail.com>
2024-01-15 14:14:58 -0500

    Update flight_search.py
    import requests
    change code for getdestcode
    set endpoint, headers, and query
    get a response
    make a result via json, specific to locations 
    then grab the results[0][code]
    return it
    
    the project now prints the iata codes for all cities in our sheet file instead of testing (delete testing entries first)
    
    (working)

--- Day39&40-FlightCapstone/flight_search.py
+++ Day39&40-FlightCapstone/flight_search.py
@@ -1,5 +1,7 @@
 # This class is responsible for talking to the Flight Search API.
 
+import requests
+
 TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
 TEQUILA_API_KEY = "AH-heQWgREketc"  # add your own
 
@@ -7,5 +9,10 @@ TEQUILA_API_KEY = "AH-heQWgREketc"  # add your own
 class FlightSearch:
 
     def get_destination_code(self, city_name):
-        code = "TESTING"
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
         return code
