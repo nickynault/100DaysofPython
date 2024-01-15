e68edf578cd59b5409c768e9d7eb8c416aff508c
Nicholas A <nickarsenault18@gmail.com>
2024-01-15 13:58:57 -0500

    Update flight_search.py
    added tequila endpoint/key
    started class
    get_destination_code fills out file with TESTING temporarily
    (working)

--- Day39&40-FlightCapstone/flight_search.py
+++ Day39&40-FlightCapstone/flight_search.py
@@ -1,4 +1,11 @@
 # This class is responsible for talking to the Flight Search API.
 
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
+TEQUILA_API_KEY = "AH-heQWgREketc"  # add your own
+
+
 class FlightSearch:
-    pass
+
+    def get_destination_code(self, city_name):
+        code = "TESTING"
+        return code
