17e1a22ea8948c782bd6c3054473ae5764712167
Nicholas A <nickarsenault18@gmail.com>
2024-02-09 14:24:12 -0500

    Update main.py
    finish project
    scrape site for titles,
    reverse order
    add to file
    
    (working, complete)

--- Day45-100MoviesWebsite/main.py
+++ Day45-100MoviesWebsite/main.py
@@ -2,6 +2,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get("https://")
+response = requests.get("https://empireonline.com/movies/features/best-movies-2")
+contents = response.text
 
-soup = BeautifulSoup(input, 'html.parser')
+soup = BeautifulSoup(contents, 'html.parser')
+movie_titles = [title.getText() for title in soup.find_all("h3")]
+movie_titles.reverse()
+print(movie_titles)
+
+with open("output.txt", "w", encoding="utf-8") as file:
+    for item in movie_titles:
+        file.write(str(item) + '\n')
