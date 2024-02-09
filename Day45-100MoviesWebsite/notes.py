9e6b7d91c78527334492759e9343da8253a51443
Nicholas A <nickarsenault18@gmail.com>
2024-02-09 13:17:29 -0500

    Create notes.py
    all notes so far (working)

---
+++ Day45-100MoviesWebsite/notes.py
@@ -0,0 +1,35 @@
+# 100MovieWebsite using Beautiful Soup https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+from bs4 import BeautifulSoup
+
+
+with open("website.html", encoding="utf-8") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, 'html.parser')
+
+# print(soup.title.name)
+# print(soup.prettify())
+
+# print(soup.p)
+
+# all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
+#
+# for tag in all_anchor_tags:
+#     # print(tag.getText())
+#     print(tag.get("href"))
+
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+#
+# section_heading = soup.find(name="h3", class_="heading")
+# print(section_heading.getText())
+
+# company_url = soup.select_one(selector="p a")
+# print(company_url)
+#
+# name = soup.select_one(selector="#name")
+# print(name)
+#
+# headings = soup.select(".heading")
+# print(headings)
