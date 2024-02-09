995dd4815c57bccd0bbf3d705092e81d1d06c4d9
Nicholas A <nickarsenault18@gmail.com>
2024-02-09 13:02:23 -0500

    Update main.py
    checking out beautiful soup, practicing printing stuff 
    (working)

--- Day45-100MoviesWebsite/main.py
+++ Day45-100MoviesWebsite/main.py
@@ -1,1 +1,13 @@
+# 100MovieWebsite using Beautiful Soup https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+from bs4 import BeautifulSoup
 
+
+with open("website.html", encoding="utf-8") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, 'html.parser')
+
+# print(soup.title.name)
+# print(soup.prettify())
+
+# print(soup.p)
