a23f1e541689a1ed86307c9521d8c4925ac74fd8
Nicholas A <nickarsenault18@gmail.com>
2024-02-09 13:17:13 -0500

    Update main.py
    remove some and send to notes.py

--- Day45-100MoviesWebsite/main.py
+++ Day45-100MoviesWebsite/main.py
@@ -6,8 +6,3 @@ with open("website.html", encoding="utf-8") as file:
     contents = file.read()
 
 soup = BeautifulSoup(contents, 'html.parser')
-
-# print(soup.title.name)
-# print(soup.prettify())
-
-# print(soup.p)
