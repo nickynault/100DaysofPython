42ac788a446a045841730917e5bee426b2bcd8a2
Nicholas A <nickarsenault18@gmail.com>
2024-02-12 11:32:53 -0500

    Update main.py
    add emailing capabilities
    (working, complete)

--- Day47-AmazonPriceTracker/main.py
+++ Day47-AmazonPriceTracker/main.py
@@ -1,10 +1,11 @@
 # Amazon Price Tracker w Beautiful Soup
 
 import requests
-import lxml
 from bs4 import BeautifulSoup
+import smtplib
 
 AMAZON_URL = "https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1"
+BUY_PRICE = 200
 
 response = requests.get(AMAZON_URL,
                         headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0",
@@ -18,5 +19,17 @@ start_price = soup.find(class_="a-price-whole")
 price_fraction = soup.find(class_="a-price-fraction")
 price = start_price.text + price_fraction.text
 price = float(price)
-print(price)
 
+title = soup.find(id="productTitle").get_text().strip()
+
+if price < BUY_PRICE:
+    message = f"{title} is now {price}!"
+
+    with smtplib.SMTP(YOUR_SMTP_ADDRESS, port=587) as connection:
+        connection.starttls()
+        result = connection.login(YOUR_EMAIL, YOUR_PASSWORD)
+        connection.sendmail(
+            from_addr=YOUR_EMAIL,
+            to_addrs=YOUR_EMAIL,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{AMAZON_URL}".encode("utf-8")
+        )
