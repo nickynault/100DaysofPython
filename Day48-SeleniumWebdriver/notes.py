204b7f41dfefbf4caf45134e1851165ae8024761
Nicholas A <nickarsenault18@gmail.com>
2024-02-16 15:32:59 -0500

    Update notes.py
    first round of notes

--- Day48-SeleniumWebdriver/notes.py
+++ Day48-SeleniumWebdriver/notes.py
@@ -1,1 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument(f"user-agent={USER_AGENT}")
+chrome_options.add_experimental_option("detach", True)  # keeps chrome open
+
+driver = webdriver.Chrome(options=chrome_options)
+
+# driver.get("https://www.amazon.com/Instant-Pot-Plus-60-Programmable/dp/B01NBKTPTS/?th=1")
+driver.get("https://www.python.org")
+
+# price_dollar = driver.find_element(By.CLASS_NAME, "a-price-whole")
+# price_cents = driver.find_element(By.CLASS_NAME, "a-price-fraction")
+# print(f"The price is {price_dollar.text}.{price_cents.text}")
+
+# search_bar = driver.find_element(By.NAME, value="q")
+# print(search_bar.get_attribute("placeholder"))
+# button = driver.find_element(By.ID, value="submit")
+# print(button.size)
+# doc_link = driver.find_element(By.CSS_SELECTOR, value=".documentation-widget a")
+# print(doc_link.text)
+# bug_link = driver.find_element(By.XPATH, value='//*[@id="site-map"]/div[2]/div/ul/li[3]/a')  # THIS IS OP USE THIS FOR REAL
+# print(bug_link.text)
+
+driver.quit()
