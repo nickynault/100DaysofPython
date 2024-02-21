3a2785ff78bc9796ff7035b6ee713333f991c98d
Nicholas A <nickarsenault18@gmail.com>
2024-02-21 13:54:08 -0500

    Update main.py
    part 2 done, applies for a job
    (working)

--- Day49-LinkedInProject/main.py
+++ Day49-LinkedInProject/main.py
@@ -8,9 +8,9 @@ import time
 JOB_BOARD = ("https://www.linkedin.com/jobs/search/?currentJobId=3812214541&distance=25&f_AL=true&f_E=1%2C2%2C3&f_"
              "I=4%2C96%2C6%2C118%2C3231%2C24&f_JT=F%2CP%2CT%2CI&f_SB2=2&f_TPR=r2592000&f_WT=1%2C3%2C2&geoId=1029652"
              "50&keywords=software%20engineer&origin=JOB_SEARCH_PAGE_JOB_FILTER&refresh=true&sortBy=R")
-EMAIL = "nickarsenault18@gmail.com"
-PASSWORD = "LinkdMyNuts123!"
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"
+EMAIL = "email"
+PASSWORD = "pass"
+USER_AGENT = "Mozilla/5.0 ..."
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument(f"user-agent={USER_AGENT}")
@@ -32,3 +32,17 @@ email_box.send_keys(EMAIL)
 password_box.send_keys(PASSWORD)
 password_box.send_keys(Keys.ENTER)
 
+# Find the apply button
+time.sleep(5)
+apply_button = driver.find_element(by=By.CSS_SELECTOR, value=".jobs-s-apply button")
+apply_button.click()
+
+# If application requires phone number and the field is empty, then fill in the number.
+time.sleep(5)
+phone = driver.find_element(by=By.CSS_SELECTOR, value="input[id*=phoneNumber]")
+if phone.text == "":
+    phone.send_keys(PHONE)
+
+# Submit the application
+submit_button = driver.find_element(by=By.CSS_SELECTOR, value="footer button")
+submit_button.click()
