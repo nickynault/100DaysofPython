b99e85231817fd80c5131b02b4ececa6bc1034a4
Nicholas A <nickarsenault18@gmail.com>
2024-02-23 16:33:09 -0500

    Update main.py
    part 1, setup
    get facebook login
    grab tinder website
    login with facebook
    (working)

--- Day50-TinderSwiper/main.py
+++ Day50-TinderSwiper/main.py
@@ -1,1 +1,22 @@
+# Auto tinder swiper
+
+from selenium import webdriver
+from time import sleep
+
+FB_EMAIL = "YOUR FACEBOOK LOGIN EMAIL"
+FB_PASSWORD = "YOUR FACEBOOK PASSWORD"
+
+chrome_driver_path = "YOUR CHROME DRIVER PATH"
+driver = webdriver.Chrome()
+
+driver.get("http://www.tinder.com")
+
+sleep(2)
+login_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/header/div[1]/div[2]/div/button')
+login_button.click()
+
+sleep(2)
+fb_login = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+fb_login.click()
+
 
