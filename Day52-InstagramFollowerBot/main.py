313becfdc1ab79b3cf95add5f8a6d86cc754495f
Nicholas A <nickarsenault18@gmail.com>
2024-04-07 13:57:14 -0400

    Update main.py
    def login completed
    added imports
    working

--- Day52-InstagramFollowerBot/main.py
+++ Day52-InstagramFollowerBot/main.py
@@ -1,6 +1,9 @@
 # Instagram Follower Bot
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 
 SIMILAR_ACCOUNT = "INSTAGRAM ACCOUNT YOU WANT TO BECOME"
 USERNAME = "YOUR INSTAGRAM EMAIL"
@@ -16,7 +19,37 @@ class InstaFollower:
         self.driver = webdriver.Chrome(options=chrome_options)
 
     def login(self):
-        pass
+        url = "https://www.instagram.com/accounts/login/"
+        self.driver.get(url)
+        time.sleep(4.2)
+
+        # Check if the cookie warning is present on the page
+        decline_cookies_xpath = "/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[2]"
+        cookie_warning = self.driver.find_elements(By.XPATH, decline_cookies_xpath)
+        if cookie_warning:
+            # Dismiss the cookie warning by clicking an element or button
+            cookie_warning[0].click()
+
+        username = self.driver.find_element(by=By.NAME, value="username")
+        password = self.driver.find_element(by=By.NAME, value="password")
+
+        username.send_keys(USERNAME)
+        password.send_keys(PASSWORD)
+
+        time.sleep(2.1)
+        password.send_keys(Keys.ENTER)
+
+        time.sleep(4.3)
+        # Click "Not now" and ignore Save-login info prompt
+        save_login_prompt = self.driver.find_element(by=By.XPATH, value="//div[contains(text(), 'Not now')]")
+        if save_login_prompt:
+            save_login_prompt.click()
+
+        time.sleep(3.7)
+        # Click "not now" on notifications prompt
+        notifications_prompt = self.driver.find_element(by=By.XPATH, value="// button[contains(text(), 'Not Now')]")
+        if notifications_prompt:
+            notifications_prompt.click()
 
     def find_followers(self):
         pass
