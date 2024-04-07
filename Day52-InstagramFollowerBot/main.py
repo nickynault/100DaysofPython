32bf943367a8f9c36d436ff777c35edec40a6ed1
Nicholas A <nickarsenault18@gmail.com>
2024-04-07 14:01:57 -0400

    Update main.py
    find followers complete with detailed explanation
    working

--- Day52-InstagramFollowerBot/main.py
+++ Day52-InstagramFollowerBot/main.py
@@ -52,7 +52,21 @@ class InstaFollower:
             notifications_prompt.click()
 
     def find_followers(self):
-        pass
+        time.sleep(5)
+        # Show followers of the selected account. 
+        self.driver.get(f"https://www.instagram.com/{SIMILAR_ACCOUNT}/followers")
+
+        time.sleep(5.2)
+        # The xpath of the modal that shows the followers will change over time. Update yours accordingly.
+        modal_xpath = "/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]"
+        modal = self.driver.find_element(by=By.XPATH, value=modal_xpath)
+        for i in range(10):
+            # In this case we're executing some Javascript, that's what the execute_script() method does.
+            # The method can accept the script as well as an HTML element.
+            # The modal in this case, becomes the arguments[0] in the script.
+            # Then we're using Javascript to say: "scroll the top of the modal (popup) element by the height of the modal (popup)"
+            self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
+            time.sleep(2)
 
     def follow(self):
         pass
