3740b0066d4f4f554bf7dd250f2dd9decfd54d3a
Nicholas A <nickarsenault18@gmail.com>
2024-04-07 13:42:08 -0400

    Update main.py
    part 1, imports, constants, class instafollower, set bot up

--- Day52-InstagramFollowerBot/main.py
+++ Day52-InstagramFollowerBot/main.py
@@ -1,1 +1,32 @@
+# Instagram Follower Bot
+
+from selenium import webdriver
+
+SIMILAR_ACCOUNT = "INSTAGRAM ACCOUNT YOU WANT TO BECOME"
+USERNAME = "YOUR INSTAGRAM EMAIL"
+PASSWORD = "YOUR INSTAGRAM PASSWORD"
+
+
+class InstaFollower:
+
+    def __init__(self):
+        # Optional - Keep browser open (helps diagnose issues during a crash)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(options=chrome_options)
+
+    def login(self):
+        pass
+
+    def find_followers(self):
+        pass
+
+    def follow(self):
+        pass
+
+
+bot = InstaFollower()
+bot.login()
+bot.find_followers()
+bot.follow()
 
