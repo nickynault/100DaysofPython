d1b28ef586cee68ee7fe3fcf7c8a152a04b320ec
Nicholas A <nickarsenault18@gmail.com>
2024-04-10 19:13:00 -0400

    Update hello.py
    runs by itself now, without command prompt entries

--- Day54-PythonDecorator/hello.py
+++ Day54-PythonDecorator/hello.py
@@ -1,8 +1,16 @@
+# https://flask.palletsprojects.com/en/3.0.x/quickstart/
+# type in terminal set FLASK_APP=hello.py once upon starting this project, then flask run (in command prompt, not local
+# https://www.cs.columbia.edu/~sedwards/classes/2015/1102-fall/Command%20Prompt%20Cheatsheet.pdf
 from flask import Flask
 
 app = Flask(__name__)
+
+print(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run()
